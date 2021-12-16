7b80f75d43d4105387503c0bff6d8486feeb5163
saharabanu <saharabanungn@gmail.com>
2021-12-16 23:46:47 +0600

    admin api added

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
         const productsCollection = database.collection('products');
         const ordersCollection= database.collection("orders");
         const reviewsCollection = database.collection('Reviews');
+        const usersCollection = database.collection('Users');
 
            // get api  
            app.get('/products',async(req,res)=>{
@@ -150,6 +151,43 @@ app.get('/catagoriesOrder', async (req, res) => {
     const cursor = reviewsCollection.find({});
     const result = await cursor.toArray();
     res.json(result)
+});
+
+
+// user post api 
+app.post('/users', async (req, res) => {
+  const user = req.body;
+  const result = await usersCollection.insertOne(user);
+  res.json(result);
+});
+app.put('/users', async (req, res) => {
+  const user = req.body;
+  const filter = { email: user.email };
+  const options = { upsert: true };
+  const updateDoc = { $set: user };
+  const result = await usersCollection.updateOne(filter, updateDoc, options);
+  res.json(result);
+});
+// search admin api 
+app.get('/users/:email', async (req, res) => {
+  const email = req.params.email;
+  const query = { email: email };
+  const user = await usersCollection.findOne(query);
+  let isAdmin = false;
+  if (user?.role === 'admin') {
+      isAdmin = true;
+  }
+  res.json({ admin: isAdmin });
+})
+
+ // make admin 
+ app.put('/users/admin', async(req,res)=>{
+  const user = req.body;
+  const filter = {email: user.email};
+  const updateDoc = { $set: { role: 'admin'}};
+  const result = await usersCollection.updateOne(filter,updateDoc);
+  console.log(result)
+  res.json(result)
 })
 
         
