9b8875135676dd4f1d6bd6cad859b794728f100d
saharabanu <saharabanungn@gmail.com>
2021-12-17 08:13:03 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const admin = require("firebase-admin");
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 const { MongoClient } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 
+// cctv-camera-ee776-firebase-adminsdk.json
+
+
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+// require('./cctv-camera-ee776-firebase-adminsdk.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jy11d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function verifyToken (req, res, next){
+
+  if(req.headers?.authorization?.startsWith(`Bearer `)){
+    const token = req.headers.authorization.split(' ')[1];
+      try{
+          const decodedUser = await admin.auth().verifyIdToken(token);
+          req.decodedEmail = decodedUser.email;
+      }
+      catch{
+
+      }
+    
+  }
+    next();
+}
 
 async function run() {
     try{
@@ -97,7 +125,7 @@ async function run() {
             status: newStatus.Status
         }
     }
-    const result = await orders.updateOne(filter, updatePackage, options)
+    const result = await ordersCollection.updateOne(filter, updatePackage, options)
     res.json(result)
 })
 
@@ -107,9 +135,9 @@ app.get('/catagoriesOrder', async (req, res) => {
   const query = { status: status };
   let cursor = {}
   if (status) {
-      cursor = orders.find(query);
+      cursor = ordersCollection.find(query);
   } else {
-      cursor = orders.find({});
+      cursor = ordersCollection.find({});
   }
   const result = await cursor.toArray();
   res.json(result)
@@ -181,13 +209,23 @@ app.get('/users/:email', async (req, res) => {
 })
 
  // make admin 
- app.put('/users/admin', async(req,res)=>{
+ app.put('/users/admin',verifyToken, async(req,res)=>{
   const user = req.body;
-  const filter = {email: user.email};
-  const updateDoc = { $set: { role: 'admin'}};
-  const result = await usersCollection.updateOne(filter,updateDoc);
-  console.log(result)
-  res.json(result)
+  const requester= req.decodedEmail
+  if(requester){
+    const requesterAccount = await usersCollection.findOne({email:requester});
+    if(requesterAccount.role === 'admin'){
+      const filter = {email: user.email};
+      const updateDoc = { $set: { role: 'admin'}};
+      const result = await usersCollection.updateOne(filter,updateDoc);
+      console.log(result)
+      res.json(result)
+    }
+  }
+  else{
+    res.status(403).json({message:'You do not have permission to make an admin'})
+  }
+  
 })
 
         
